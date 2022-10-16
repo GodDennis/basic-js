ac72e0774186c8b1f94e084712e924d5e2192448
GodDennis <denpodreckiy@mail.ru>
2022-10-16 15:34:05 +0300

    createDreamTeam passed

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (members === null || members === undefined) {
+    return false
+  }
+  let membersName = [];
+  let result = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) === "string") {
+      membersName.push(members[i].trim())
+    }
+  }
+  for (let i = 0; i < membersName.length; i++) {
+    result.push(membersName[i][0].toUpperCase())
+  }
+
+  return (result.sort().join(''));
+
 }
 
 module.exports = {
